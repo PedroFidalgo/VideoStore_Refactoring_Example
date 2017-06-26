c9da50acf53c0873ee7e2e0f41c81ed2df4f68a1
pedrofidalgo <pedro.fidalgo@tecnico.ulisboa.pt>
2017-06-26 13:18:22 +0100

    Move method - getCharge
    Moved the logic in ammountFor int the Customer Class to the new method
    getCharge in the Rental class for better cohesion.

--- src/main/java/org/videostore/Customer.java
+++ src/main/java/org/videostore/Customer.java
@@ -47,22 +47,6 @@ public class Customer {
 	}
 
 	private double ammountFor(Rental rental) {
-		double thisAmount = 0;
-		switch (rental.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (rental.getDaysRented() > 2)
-					thisAmount += (rental.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += rental.getDaysRented() * 3;
-				break;
-			case Movie.CHILDREN:
-				thisAmount += 1.5;
-				if (rental.getDaysRented() > 3)
-					thisAmount += (rental.getDaysRented() - 3) * 1.5;
-				break;
-		}
-		return thisAmount;
+		return rental.getCharge();
 	}
 }
