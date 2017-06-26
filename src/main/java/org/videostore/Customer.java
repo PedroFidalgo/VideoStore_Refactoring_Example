293ea7dbfcdafedb81a26f914f351d2d33aefd6c
pedrofidalgo <pedro.fidalgo@tecnico.ulisboa.pt>
2017-06-26 13:09:38 +0100

    Extract Method - amountFor

--- src/main/java/org/videostore/Customer.java
+++ src/main/java/org/videostore/Customer.java
@@ -21,26 +21,12 @@ public class Customer {
 	public String statement() {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
+		double thisAmount = 0;
 		String result = "Rental Record for " + getName() + "\n";
 		for (Rental rental : rentals) {
-			double thisAmount = 0;
 			
 			//determine amounts for each line
-			switch (rental.getMovie().getPriceCode()) {
-				case Movie.REGULAR:
-					thisAmount += 2;
-					if (rental.getDaysRented() > 2)
-						thisAmount += (rental.getDaysRented() - 2) * 1.5;
-					break;
-				case Movie.NEW_RELEASE:
-					thisAmount += rental.getDaysRented() * 3;
-					break;
-				case Movie.CHILDREN:
-					thisAmount += 1.5;
-					if (rental.getDaysRented() > 3)
-						thisAmount += (rental.getDaysRented() - 3) * 1.5;
-					break;
-			}
+			thisAmount = ammountFor(rental);
 			// add frequent renter points
 			frequentRenterPoints ++;
 			// add bonus for a two day new release rental
@@ -58,5 +44,25 @@ public class Customer {
 		result += "You earned " + String.valueOf(frequentRenterPoints)
 		+ " frequent renter points";
 		return result;
+	}
+
+	private double ammountFor(Rental rental) {
+		double thisAmount = 0;
+		switch (rental.getMovie().getPriceCode()) {
+			case Movie.REGULAR:
+				thisAmount += 2;
+				if (rental.getDaysRented() > 2)
+					thisAmount += (rental.getDaysRented() - 2) * 1.5;
+				break;
+			case Movie.NEW_RELEASE:
+				thisAmount += rental.getDaysRented() * 3;
+				break;
+			case Movie.CHILDREN:
+				thisAmount += 1.5;
+				if (rental.getDaysRented() > 3)
+					thisAmount += (rental.getDaysRented() - 3) * 1.5;
+				break;
+		}
+		return thisAmount;
 	}
 }
